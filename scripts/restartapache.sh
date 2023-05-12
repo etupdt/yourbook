15fd8eb3c5548dfac246275f502b427ba8fdd4b6
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-05-12 21:18:49 +0200

    correction cacert file path 9

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -6,5 +6,5 @@ echo 'exec restartapache.sh' |& sudo tee $log
 
 aws ecr get-login-password --region eu-west-3 | sudo docker login --username AWS --password-stdin 498746666064.dkr.ecr.eu-west-3.amazonaws.com
 
-sudo docker pull 498746666064.dkr.ecr.eu-west-3.amazonaws.com/yourbook:latest
-sudo docker run -p 8443:8443 498746666064.dkr.ecr.eu-west-3.amazonaws.com/yourbook
+sudo docker pull 498746666064.dkr.ecr.eu-west-3.amazonaws.com/angular-yourbook:latest
+sudo docker run -p 443:443 498746666064.dkr.ecr.eu-west-3.amazonaws.com/angular-yourbook

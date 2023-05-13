1279bb382cf8fd97df018b88c43904fa72e89d21
Denis Tavernier <denis-tavernier@wanadoo.fr>
2023-05-13 08:48:08 +0200

    correction apache httpd parameters 2

--- scripts/restartapache.sh
+++ scripts/restartapache.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-log=/var/log/deploy/restartapache_synffront.log
+log=/var/log/deploy/angular-yourbook.log
 
 echo 'exec restartapache.sh' |& sudo tee $log
 
-aws ecr get-login-password --region eu-west-3 | sudo docker login --username AWS --password-stdin 498746666064.dkr.ecr.eu-west-3.amazonaws.com
+aws ecr get-login-password --region eu-west-3 | sudo docker login --username AWS --password-stdin 498746666064.dkr.ecr.eu-west-3.amazonaws.com |& tee $log
 
-sudo docker pull 498746666064.dkr.ecr.eu-west-3.amazonaws.com/angular-yourbook:latest
-sudo docker run -p 443:443 498746666064.dkr.ecr.eu-west-3.amazonaws.com/angular-yourbook
+sudo docker pull 498746666064.dkr.ecr.eu-west-3.amazonaws.com/angular-yourbook:latest |& tee -a $log
+sudo docker run -p 443:443 498746666064.dkr.ecr.eu-west-3.amazonaws.com/angular-yourbook |& tee -a $log
